733ed819f4ae5a36a0946e008e4ea141a6ddf0fa
korrio <iamnotkorr@gmail.com>
2024-11-17 07:02:58 +0700

    init

---
+++ index.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" type="image/png" sizes="16x16" href="plugins/images/imperialsofetch.png">
+    <title>sourcetrack.co</title>
+    <!-- Bootstrap Core CSS -->
+    <link href="plugins/bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- <link href="plugins/bower_components/bootstrap-extension/css/bootstrap-extension.css" rel="stylesheet"> -->
+    <!-- animation CSS -->
+    <link href="css/animate.css" rel="stylesheet">
+    <!-- Custom CSS -->
+    <link href="css/style.css" rel="stylesheet">
+    <!-- color CSS -->
+    <link href="css/colors/blue.css" id="theme" rel="stylesheet">
+
+    <style>
+        body .login-register {
+            /*background-image:url("https://images.unsplash.com/photo-1524350876685-274059332603?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2851&q=80") !important;*/
+            background-image:none !important;
+        }
+
+    </style>
+
+</head>
+
+<body>
+    <!-- Preloader -->
+    <div class="preloader">
+        <div class="cssload-speeding-wheel"></div>
+    </div>
+    <section id="wrapper" class="login-register">
+        <div class="login-box">
+            <div class="white-box">
+                    <div class="form-group text-center">
+                        <div class="col-xs-12">
+
+                             <img src="https://nouns.center/earth.gif" style="width:300px; height: 300px;"  />
+                        </div>
+                    </div>
+                    <div class="form-group  text-center">
+                        <div class="col-xs-12">
+                           <h4><b>Welcome to folktag.co</b></h4>
+                            <h6>Agricultural Supplychain Traceability</h6>
+                            <img width="240px" src="https://www.techhub.in.th/wp-content/uploads/2022/07/09.bitkub-chain-logo.png" />
+
+                        </div>
+                    </div>
+                    <div class="form-group text-center">
+                        <div class="col-xs-6 p-b-20">
+                            <a href="admin.php" target="_blank" class="btn btn-rounded btn-block btn-primary">Admin</a>
+                        </div>
+                         <div class="col-xs-6 p-b-20">
+                            <a href="user.php" class="btn btn-rounded btn-block btn-info">Log In</a>
+                        </div>
+                    </div>
+            </div>
+        </div>
+    </section>
+    <!-- jQuery -->
+    <script src="plugins/bower_components/jquery/dist/jquery.min.js"></script>
+    <!-- Bootstrap Core JavaScript -->
+    <script src="plugins/bower_components/bootstrap/dist/js/tether.min.js"></script>
+    <script src="plugins/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="plugins/bower_components/bootstrap-extension/js/bootstrap-extension.min.js"></script>
+    <!-- Menu Plugin JavaScript -->
+    <script src="plugins/bower_components/sidebar-nav/dist/sidebar-nav.min.js"></script>
+    <!--slimscroll JavaScript -->
+    <script src="js/jquery.slimscroll.js"></script>
+    <!--Wave Effects -->
+    <script src="js/waves.js"></script>
+    <!-- Custom Theme JavaScript -->
+    <script src="js/custom.min.js"></script>
+    <!--Style Switcher -->
+</body>
+
+</html>
---
+++ view-batch.php
@@ -0,0 +1,202 @@
+<?php include 'templates/_header.php';?>
+<?php
+    if (! isset($_GET['batchNo']) || (isset($_GET['batchNo']) && $_GET['batchNo'] == '') &&
+        ! isset($_GET['txn']) || (isset($_GET['txn']) && $_GET['txn'] == '')) {
+        echo "<script>window.location = 'index.php';</script>";
+    }
+?>
+<style type="text/css">
+    .verified_info{
+        color: green;
+    }
+</style>
+<style type="text/css">
+    .verified_info{
+        color: green;
+    }
+    .white-box{
+        padding: 0;
+        background: #edf1f5;
+    }
+    .no-gutters {
+        margin-right: 0 !important;
+        margin-left: 0 !important;
+    }
+    .timeline>li>.timeline-panel{
+        background-color: #fff;
+        color: #2b2b2b;
+    }
+    .timeline:before {
+        background-color: rgba(0, 0, 0, 0.1);
+    }
+    .activityQrCode{
+        margin: 20px 0;
+    }
+    .table>tbody>tr:first-child>td{
+        border-top: none;
+    }
+    .activityDateTime{
+        font-size: 12px;
+    }
+    .activityDateTime .text-info{
+        overflow: hidden;
+    }
+</style>
+<div class="container-fluid">
+        <div class="row bg-title">
+            <div class="col-lg-6 col-md-4 col-sm-4 col-xs-12">
+                <h3 class="page-title">Batch Progress <a href="javascript:void(0);" onclick="javascript:window.print();" class="text-info" title="Print Page Report"><i class="fa fa-print"></i> Print</a></h3>
+                <h4><b>Batch No: </b><?php echo $_GET['batchNo']; ?></h4>
+            </div>
+            <div class="col-lg-6 col-sm-8 col-md-8 col-xs-12">
+
+            </div>
+            <!-- /.col-lg-12 -->
+        </div>
+        <!-- .row -->
+        <div class="row">
+            <div class="col-md-12">
+                <div class="white-box">
+                    <ul class="timeline">
+                        <li>
+                            <div class="timeline-badge danger">
+                                <i class="fa fa-check"></i>
+                            </div>
+                            <div class="timeline-panel" id="cultivationSection">
+                                <div class="timeline-heading">
+                                    <h4 class="timeline-title">Cultivation</h4>
+                                    <div class="text-muted text-success activityDateTime"></div>
+                                </div>
+                                <div>
+                                    <div class="activityQrCode text-center"></div>
+                                </div>
+                                <div class="timeline-body">
+                                    <table class="table activityData table-responsive" >
+                                        <tr>
+                                            <td colspan="2"><p>Information Not Available</p></td>
+                                        </tr>
+                                    </table>
+                                </div>
+                                <div class="verify-information text-right"></div>
+                            </div>
+                        </li>
+                        <li class="timeline-inverted">
+                            <div class="timeline-badge danger">
+                                <i class="fa fa-times"></i>
+                            </div>
+                            <div class="timeline-panel" id="farmInspectionSection">
+                                <div class="timeline-heading">
+                                    <h4 class="timeline-title">Farm inspection</h4>
+                                    <div class="text-muted text-success activityDateTime"></div>
+                                </div>
+                                <div>
+                                    <div class="activityQrCode text-center"></div>
+                                </div>
+                                <div class="timeline-body">
+                                    <table class="table activityData table-responsive">
+                                        <tr>
+                                            <td colspan="2"><p>Information Not Available</p></td>
+                                        </tr>
+                                    </table>
+                                </div>
+                                <div class="verify-information text-right"></div>
+                            </div>
+                        </li>
+                        <li>
+                           <div class="timeline-badge danger">
+                                <i class="fa fa-times"></i>
+                            </div>
+                            <div class="timeline-panel" id="harvesterSection">
+                                <div class="timeline-heading">
+                                    <h4 class="timeline-title">การเก็บเกี่ยว</h4>
+                                    <div class="text-muted text-success activityDateTime"></div>
+                                </div>
+                                <div>
+                                    <div class="activityQrCode text-center"></div>
+                                </div>
+                                <div class="timeline-body">
+                                    <table class="table activityData table-responsive" >
+                                        <tr>
+                                            <td colspan="2"><p>Information Not Available</p></td>
+                                        </tr>
+                                    </table>
+                                </div>
+                                <div class="verify-information text-right"></div>
+                            </div>
+                        </li>
+                        <li class="timeline-inverted">
+                            <div class="timeline-badge danger">
+                                <i class="fa fa-times"></i>
+                            </div>
+                            <div class="timeline-panel" id="exporterSection">
+                                <div class="timeline-heading">
+                                    <h4 class="timeline-title">Export</h4>
+                                    <div class="text-muted text-success activityDateTime"></div>
+                                </div>
+                                <div>
+                                    <div class="activityQrCode text-center"></div>
+                                </div>
+                                <div class="timeline-body">
+                                    <table class="table activityData table-responsive">
+                                        <tr>
+                                            <td colspan="2"><p>Information Not Available</p></td>
+                                        </tr>
+                                    </table>
+                                </div>
+                                <div class="verify-information text-right"></div>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="timeline-badge danger">
+                                <i class="fa fa-times"></i>
+                            </div>
+                            <div class="timeline-panel" id="importerSection">
+                                <div class="timeline-heading">
+                                    <h4 class="timeline-title">Import</h4>
+                                    <div class="text-muted text-success activityDateTime"></div>
+                                </div>
+                                <div>
+                                    <div class="activityQrCode text-center"></div>
+                                </div>
+                                <div class="timeline-body">
+                                   <table class="table activityData table-responsive" >
+                                        <tr>
+                                            <td colspan="2"><p>Information Not Available</p></td>
+                                        </tr>
+                                    </table>
+                                </div>
+                                <div class="verify-information text-right"></div>
+                            </div>
+                        </li>
+                        <li class="timeline-inverted">
+                            <div class="timeline-badge danger">
+                                <i class="fa fa-times"></i>
+                            </div>
+                            <div class="timeline-panel" id="processorSection">
+                                <div class="timeline-heading">
+                                    <h4 class="timeline-title">Processor</h4>
+                                    <div class="text-muted text-success activityDateTime"></div>
+                                </div>
+                                <div>
+                                    <div class="activityQrCode text-center"></div>
+                                </div>
+                                <div class="timeline-body">
+                                    <table class="table activityData table-responsive" >
+                                        <tr>
+                                            <td colspan="2"><p>Information Not Available</p></td>
+                                        </tr>
+                                    </table>
+                                </div>
+                                <div class="verify-information text-right"></div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <!-- /.row -->
+</div>
+<input type="hidden" id="batchNo" value="<?php $batchNo = isset($_GET['batchNo']) ? $_GET['batchNo'] : '';
+                                         echo $batchNo;?>">
+
+<?php include 'templates/_footer.php';?>
